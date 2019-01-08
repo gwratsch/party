285e1b2c55a617ff4975a61da7470965db8925ea
Gerd Ratsch <ratsch.gerd@gmail.com>
2019-01-08 12:29:08 +0100

    change config settings for pgsql

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 function config(){
     $database = array(
-        'dbUser' => 'admin',
-        'userPW' => 'bgt5BGT%',
-        'dbname' => 'Party91',
-        'dbtype' => 'mysql',
-        'port' => '',
-        'host' => 'localhost'
+        'dbUser' => 'jhuptycscnhbbg',
+        'userPW' => 'b6fce2e584e03fbdd29fa5a2fa2631b55fc96c2f2ecbb94f2ea9d25a558ff2e4',
+        'dbname' => 'd5jo3fn7oh3eok',
+        'dbtype' => 'pgsql',
+        'port' => '5432',
+        'host' => 'ec2-54-247-161-208.eu-west-1.compute.amazonaws.com'
     );
     
     return $database;
