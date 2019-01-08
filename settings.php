19cda654ffd46ca28c943aa9005a5b01853a623f
Gerd Ratsch <ratsch.gerd@gmail.com>
2019-01-08 15:55:28 +0100

    Update settings.php
    Replace __autoload to spl_autoload_register.

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 include_once 'modules/translation.php';
 
 //$path = configsettings::pathname();
-function __autoload($class_name) {
+spl_autoload_register(function ($class_name) {
     $pathlist = array(
         'includes',
         'templates',
@@ -11,11 +11,11 @@ function __autoload($class_name) {
     foreach($pathlist as $pathname){
         $file = $pathname.'/'.$class_name . '.php';
         if(file_exists($file)){
-            include_once $pathname.'/'.$class_name . '.php';
+            require_once $pathname.'/'.$class_name . '.php';
             break;
         }
     }
-}
+});
 
 function debuginfo($array){
     var_dump(print_r("<pre>".print_r($array,true)."</pre>"));
