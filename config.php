1acfc369c81bea3947192aa8fe71bbfa8d37139d
Gerd Ratsch <ratsch.gerd@gmail.com>
2019-01-09 14:02:09 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ function config(){
         'dbUser' => 'jhuptycscnhbbg',
         'userPW' => 'b6fce2e584e03fbdd29fa5a2fa2631b55fc96c2f2ecbb94f2ea9d25a558ff2e4',
         'dbname' => 'd5jo3fn7oh3eok',
-        'dbtype' => 'mysql',
+        'dbtype' => 'pgsql',
         'port' => '5432',
         'host' => 'ec2-54-247-161-208.eu-west-1.compute.amazonaws.com'
     );
